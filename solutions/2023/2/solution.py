9b4a8efc6f7825f833ec6464034382217cae80c7
lone_druid <enikita332@gmail.com>
2023-12-02 15:59:40 +0300

    fmt

--- solutions/2023/2/solution.py
+++ solutions/2023/2/solution.py
@@ -4,13 +4,20 @@ from math import prod
 
 limit = {"red": 12, "green": 13, "blue": 14}
 
+
 def split(string: str, delim: str) -> list[str]:
     return string.split(delim, maxsplit=1)
 
-def to_colormap(bag_info: str, hof: Callable[[dict[str, int], str, int], None] = lambda cmap, color, amount: cmap.__setitem__(color, max(cmap[color], amount))) -> dict[str, int]:
+
+def to_colormap(
+    bag_info: str,
+    hof: Callable[
+        [dict[str, int], str, int], None
+    ] = lambda cmap, color, amount: cmap.__setitem__(color, max(cmap[color], amount)),
+) -> dict[str, int]:
     result = dict.fromkeys(["red", "green", "blue"], 0)
-    for pair in map(str.strip, bag_info.replace(';',',').split(',')):
-        amount, color = split(pair, ' ')
+    for pair in map(str.strip, bag_info.replace(";", ",").split(",")):
+        amount, color = split(pair, " ")
         amount = int(amount.strip())
         color = color.strip()
         hof(result, color, amount)
@@ -23,12 +30,11 @@ class Solution:
         id_sum = 0
         for line in file:
             gid, bags = split(line, ":")
-            game_id = int(split(gid, ' ')[1].strip())
+            game_id = int(split(gid, " ")[1].strip())
             cmap = to_colormap(bags)
-            if all(cmap[color]<=limit[color] for color in cmap):
+            if all(cmap[color] <= limit[color] for color in cmap):
                 id_sum += game_id
         return id_sum
-
 
     @classmethod
     def part_2(cls: Self, file: TextIOWrapper) -> int:
