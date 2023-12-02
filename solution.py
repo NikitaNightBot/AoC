4b137d50ef22d001b9b423b8f6a20cc0acbf5bec
lone_druid <enikita332@gmail.com>
2023-12-02 16:08:07 +0300

    lmao

---
+++ solution.py
@@ -0,0 +1,12 @@
+# Template
+from io import TextIOWrapper
+from typing import Self
+
+class Solution:
+    @classmethod
+    def part_1(cls: Self, file: TextIOWrapper):
+        ...
+
+    @classmethod
+    def part_2(cls: Self, file: TextIOWrapper):
+        ...
